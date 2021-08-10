89c8c3c62ae8aae270fb712a51eccaf4e3a9a6ee
Ragulin Dmitry <dvragulin@gmail.com>
2021-08-10 18:49:00 +0300

    Update Bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -36,7 +36,7 @@ fi
 
 # --- Run playbook ---------------------------------------------------------------------------------------------------
 cd $HOME/GIT/Projects_Home/
-git clone https://github.com/dvragulin/dotfiles-public.git
+git clone git@github.com:dvragulin/dotfiles-public.git
 cd $HOME/GIT/Projects_Home/dotfiles-public
 
 ansible-playbook playbook.yml --extra-vars "ansible_sudo_pass=$PW"
