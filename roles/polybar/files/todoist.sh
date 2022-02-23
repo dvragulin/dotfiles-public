37fb483759417d40d4cc34a264ed927631b0a98f
Dmitry Ragulin <dvragulin@gmail.com>
2022-02-23 23:25:38 +0300

    Update polybar scripts

--- roles/polybar/files/todoist.sh
+++ roles/polybar/files/todoist.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 todo s
 
-TODAY_TASKS=$(todo l | awk ' {print $3} ' | rg / | wc -l)
+TODAY_TASKS=$(todo l | awk ' {print $3} ' | rg / | awk -F '/' '{print $3}' | rg $(date -u +"%d") | wc -l)
 
 P1_TASKS=$(todo l | rg "p1" | awk ' {print $1} ' | wc -l)
 IN_TASKS=$(todo l | rg "Inbox" | awk ' {print $1} ' | wc -l)
 
-echo -ne "$TODAY_TASKS $IN_TASKS $P1_TASKS"
+echo -ne " $TODAY_TASKS  $P1_TASKS  $IN_TASKS"
+
