3c53848250266af02c4c1c341bc56eae736554cd
Dmitry Ragulin <dvragulin@gmail.com>
2022-03-17 19:41:21 +0300

    Update  apps and polybar

--- roles/polybar/files/jira_get_issues.py
+++ roles/polybar/files/jira_get_issues.py
@@ -13,8 +13,10 @@ jira_client = JIRA(options={'server': f'{jira_url}'},
                    basic_auth=(f'{email}', f'{token}'))
 project = jira_client.project("INFRADEV")
 
-jql_str = f'assignee = {jira_login} AND resolution = Unresolved AND status not in ' \
-          f'(Canceled, Completed, Declined, Tested) ORDER BY Rank ASC'
+filter_1 = f'assignee = {jira_login} AND resolution = Unresolved AND status not in ' \
+           f'(Canceled, Completed, Declined, Tested) ORDER BY Rank ASC'
+filter_2 = """
+assignee in (EMPTY) AND summary ~ "Задача из Slack*" AND project = INFRADEV AND status in ("In Progress", Backlog, Blocked, Open, Reopened) AND "Assigned team[Dropdown]" = DevOps-1st-line ORDER BY created DESC
+"""
 
-total = jira_client.search_issues(jql_str)
-print(len(total))
+print(f"{len(jira_client.search_issues(filter_1))} {len(jira_client.search_issues(filter_2))}")
