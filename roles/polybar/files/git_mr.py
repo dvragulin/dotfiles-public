7a65dd0ee37c78e6b93cde07db6c126814a475d6
Dmitry Ragulin <dvragulin@gmail.com>
2022-05-17 23:08:26 +0300

    Update polybar

--- roles/polybar/files/git_mr.py
+++ roles/polybar/files/git_mr.py
@@ -10,10 +10,21 @@ token = eget('GITLAB_WORK_TOKEN')
 mr_URL = f"{gitlab}/api/v4/merge_requests/?assignee_username=dmitriy.ragulin&state=opened&access_token={token}"
 rw_URL = f"{gitlab}/api/v4/merge_requests/?reviewer_username=dmitriy.ragulin&state=opened&access_token={token}"
 
-mr_response = requests.get(url=mr_URL)
-rw_response = requests.get(url=rw_URL)
-mr_body = json.loads(mr_response.content.decode('utf-8'))
-rw_body = json.loads(rw_response.content.decode('utf-8'))
+mr_body = "false"
+rw_body = "false"
 
-print(f"{len(mr_body)}/{len(rw_body)} ")
+try:
+    mr_response = requests.get(url=mr_URL)
+    mr_body = json.loads(mr_response.content.decode('utf-8'))
+except Exception:
+    print("")
+    # print("- / - ")
+
+try:
+    rw_response = requests.get(url=rw_URL)
+    rw_body = json.loads(rw_response.content.decode('utf-8'))
+except Exception:
+    print("")
+
+print(f" {len(mr_body)}/{len(rw_body)} ")
 
