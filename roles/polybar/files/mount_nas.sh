717b00c02160bb0a5aaff4447349018400b8b785
Dmitry Ragulin <dvragulin@gmail.com>
2022-06-03 19:08:43 +0300

    Update polybar

--- roles/polybar/files/mount_nas.sh
+++ roles/polybar/files/mount_nas.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-if [ `cat /etc/mtab | grep NAS | wc -l` -eq 0 ]; then echo "轢"; else echo "歷"; fi
+if [ `cat /etc/mtab | grep NAS | wc -l` -eq 0 ]; then echo "轢"; else echo %{F\#7AB87A}歷%{F-}; fi
+
