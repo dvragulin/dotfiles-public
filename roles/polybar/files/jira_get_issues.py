0508e08f69fcd21fe1095abfe1d99bf61a2afc36
Dmitry Ragulin <dvragulin@gmail.com>
2022-06-03 18:35:28 +0300

    Update jira in polybar

--- roles/polybar/files/jira_get_issues.py
+++ roles/polybar/files/jira_get_issues.py
@@ -14,25 +14,10 @@ try:
                        token_auth=f'{token}')
     project = jira_client.project("INFRADEV")
 
-    filter_1 = f'assignee = {jira_login} AND resolution = Unresolved AND status not in ' \
+    jira_filter = f'assignee = {jira_login} AND resolution = Unresolved AND status not in ' \
                f'(Canceled, Completed, Declined, Tested) ORDER BY Rank ASC'
-    filter_2 = """
-    assignee is EMPTY AND summary ~ "Задача из MM*" AND project = INFRADEV AND status in 
-    ("In Progress", Backlog, Blocked, Open, Reopened) AND "Assigned team" = "DevOps-1st-line"  ORDER BY created DESC
-    """
-    filter_3 = """
-    (labels != "DevOps-1st-line" OR labels is EMPTY) AND assignee is EMPTY AND "Assigned team" = DevOps-1st-line 
-    AND summary !~"Задача из MM*" AND summary !~ "Запрос доступов*" AND status = "Backlog"  AND project = InfraDev 
-    """
-    filter_4 = """
-    assignee is EMPTY AND summary ~ "Запрос доступов*" AND project = INFRADEV AND status in
-    ("In Progress", Backlog, Blocked, Open, Reopened) AND "Assigned team" = "DevOps-1st-line"  ORDER BY created DESC
-    """
 
-    print(f" {len(jira_client.search_issues(filter_4))}"
-          f" {len(jira_client.search_issues(filter_3))}"
-          f" {len(jira_client.search_issues(filter_2))}"
-          f" {len(jira_client.search_issues(filter_1))}")
+    print(f"{len(jira_client.search_issues(jira_filter))}")
 
 except Exception as e:
     pass
