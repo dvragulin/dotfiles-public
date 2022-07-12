b38323e169e0f13118f93b93c10fdea1b9dbcfa4
Dmitry Ragulin <dvragulin@gmail.com>
2022-07-12 16:45:44 +0300

    Udpate todoist in polybar

--- roles/polybar/files/todoist.py
+++ roles/polybar/files/todoist.py
@@ -3,33 +3,38 @@
 # req. pip install todoist-api-python
 
 import os
+from datetime import datetime
 from todoist_api_python.api import TodoistAPI
 
-
 def beauty(inbox, priority, today):
-    return f" {today}  {priority}  {inbox}"
+    return f" {inbox}  {today}  {priority}"
+
+
+def get_due_date(task):
+    if task.due is not None:
+        due_date = str(datetime.fromisoformat(task.due.date)).split(" ")[0]
+    else:
+        due_date = None
+    return due_date
 
 
 def main(token):
     api = TodoistAPI(token)
-    inbox_count, priority_count, today_count = 0, 0, 0
+    inbox_count, today_priority_count, today_count = 0, 0, 0
+    today = datetime.today().strftime('%Y.%m.%d').replace(".", "-")
+    tasks = api.get_tasks()
 
-    try:
-        tasks = api.get_tasks()
-        for task in tasks:
-            if task.project_id == 2161168161:
-                inbox_count += 1
-            if task.priority == 4:
-                priority_count += 1
-            try:
-                if task.due.string == "today":
-                    today_count += 1
-            except Exception:
-                pass
-    except Exception as error:
-        print(error)
+    for task in tasks:
+        due_date = get_due_date(task)
+        if task.project_id == 2161168161:
+            inbox_count += 1
+        if due_date == today:
+            today_count += 1
+        if task.priority == 4:
+            if due_date == today:
+                today_priority_count += 1
 
-    print(beauty(inbox_count, priority_count, today_count))
+    print(beauty(inbox_count, today_priority_count, today_count))
 
 
 if __name__ == '__main__':
