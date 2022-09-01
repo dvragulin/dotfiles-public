ab0421e477de7a7ce03225edab411699e977ff3c
Dmitry Ragulin <dvragulin@gmail.com>
2022-09-01 16:11:25 +0300

    Some updates

--- roles/polybar/files/git_update.sh
+++ roles/polybar/files/git_update.sh
@@ -8,13 +8,14 @@ for i in `ls $HOME/GIT/`; do
   for x in `ls $HOME/GIT/$i`; do
     cd $HOME/GIT/$i/$x
     echo
-    echo "-----------------------------------------------------------------------"
-    pwd
+    echo " -----------------------------------------------------------------------------------"
+    echo " $(pwd)"
     echo
     git co master && git pull || true | 1>/dev/null
-    sleep 10
+    echo
+    sleep 5
   done ;
 done
 
- notify-send -a "Git:" -u normal "Git repo's update completed."
+notify-send -a "Git:" -u normal "Git repo's update completed."
 
