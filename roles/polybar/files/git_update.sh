9daf47fc56cf92e72aae9630b69a7e743cd316cd
Dmitry Ragulin <dvragulin@gmail.com>
2022-12-04 23:56:00 +0300

    Update polybar

--- roles/polybar/files/git_update.sh
+++ roles/polybar/files/git_update.sh
@@ -4,18 +4,20 @@ cd $HOME/GIT/
 
 notify-send -a "Git:" -u normal "Git repo's update started."
 
-for i in `ls $HOME/GIT/`; do
-  for x in `ls $HOME/GIT/$i`; do
-    cd $HOME/GIT/$i/$x
-    echo
-    echo " -----------------------------------------------------------------------------------"
-    echo " $(pwd)"
-    echo
-    git co master && git pull || true | 1>/dev/null
-    echo
-    sleep 5
-  done ;
-done
+ansible-playbook ~/GIT/projects_home/_ansible-collection/ansible-git-control/playbook.yml
+
+# for i in `ls $HOME/GIT/`; do
+#   for x in `ls $HOME/GIT/$i`; do
+#     cd $HOME/GIT/$i/$x
+#     echo
+#     echo " -----------------------------------------------------------------------------------"
+#     echo " $(pwd)"
+#     echo
+#     git co master && git pull || true | 1>/dev/null
+#     echo
+#     sleep 5
+#   done ;
+# done
 
 notify-send -a "Git:" -u normal "Git repo's update completed."
 
