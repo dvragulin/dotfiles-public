ac684af3054c23d4029d59ba92daa3db5e398319
Dmitry Ragulin <dvragulin@gmail.com>
2022-12-07 16:57:32 +0300

    Update polybar

--- roles/polybar/files/git_update.sh
+++ roles/polybar/files/git_update.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/bash
 
-cd $HOME/GIT/
-
 notify-send -a "Git:" -u normal "Git repo's update started."
 
+TIME_START=$(date +%s)
 ansible-playbook ~/GIT/projects_home/_ansible-collection/ansible-git-control/playbook.yml
+TIME_END=$(date +%s)
 
-# for i in `ls $HOME/GIT/`; do
-#   for x in `ls $HOME/GIT/$i`; do
-#     cd $HOME/GIT/$i/$x
-#     echo
-#     echo " -----------------------------------------------------------------------------------"
-#     echo " $(pwd)"
-#     echo
-#     git co master && git pull || true | 1>/dev/null
-#     echo
-#     sleep 5
-#   done ;
-# done
+DIFF=$(( $TIME_END - $TIME_START ))
+echo "It took $DIFF seconds"
 
-notify-send -a "Git:" -u normal "Git repo's update completed."
+TIME_DIFF=$(date -d@$DIFF -u +%H:%M:%S)
+
+notify-send -a "Git:" -u normal "Update completed Time:[$TIME_DIFF]"
 
