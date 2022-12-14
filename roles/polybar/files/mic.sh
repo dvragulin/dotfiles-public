772f5475d09dc6c86fd3b0e1e7695cf43e77df09
Dmitry Ragulin <dvragulin@gmail.com>
2022-12-14 10:43:31 +0300

    Update mic

--- roles/polybar/files/mic.sh
+++ roles/polybar/files/mic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VAR=$(amixer | rg "Mono: Capture" | rg "\[on\]" | awk '{print $5}')
-if  [[ $VAR = "[on]" ]]; then echo ""; else echo %{F\#FF0000} %{F-}; fi
+if  [[ $VAR = "[on]" ]]; then echo %{F\#7AB87A}; else echo ""; fi
 
 #amixer set Capture toggle
