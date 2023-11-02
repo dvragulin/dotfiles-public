6403d0b694d89d51f5e0708cde6c2dbd63e53312
Dmitry Ragulin <dvragulin@gmail.com>
2023-11-02 10:18:55 +0300

    Update git update rc

--- roles/polybar/files/git_update.sh
+++ roles/polybar/files/git_update.sh
@@ -4,7 +4,7 @@ notify-send -a "Git" -u normal "Git repo's update started."
 
 TIME_START=$(date +%s)
 
-ansible-playbook ~/GIT/projects_home/_ansible-collection/ansible-git-control/playbook.yml | tee $HOME/.ansible.git_control.log
+ansible-playbook ~/GIT/projects_home/_ansible-collection/ansible-git-control/playbook.yml | tee $HOME/tmp/ansible.git_control.log
 
 TIME_END=$(date +%s)
 
@@ -27,5 +27,10 @@ notify-send -a "Git" -u normal "Repositories have been updated:
 Time spent:[$TIME_DIFF]
 "
 
-rg FAIL -B 1 < $HOME/.ansible.git_control.log > $HOME/tmp/ansible.git_control.errors.log
+rg FAIL -B 1 < $HOME/tmp/ansible.git_control.log > $HOME/tmp/ansible.git_control.errors.log
+
+echo "Relevant:  [$RELEVANT]" >> $HOME/tmp/ansible.git_control.log
+echo "Changed:   [$CHANGED]" >> $HOME/tmp/ansible.git_control.log
+echo "Failed:    [$FAILED]" >> $HOME/tmp/ansible.git_control.log
+echo "Time spent:[$TIME_DIFF]" >> $HOME/tmp/ansible.git_control.log
 
