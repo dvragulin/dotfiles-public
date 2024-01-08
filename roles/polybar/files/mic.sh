ffd83d0dbd28808b6980f568d64a710552793126
Dmitry Ragulin <dvragulin@gmail.com>
2024-01-08 13:26:02 +0300

    Add yandex station connect via polybar

--- roles/polybar/files/mic.sh
+++ roles/polybar/files/mic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VAR=$(amixer | rg "Mono: Capture" | rg "\[on\]" | awk '{print $5}')
-if  [[ $VAR = "[on]" ]]; then echo %{F\#7AB87A}; else echo ""; fi
+VAR=$(amixer -c 1| rg "Mono: Capture" | rg "\[on\]" | awk '{print $6}')
+if  [[ $VAR = "[on]" ]]; then echo ; else echo %{F\#cebaba}; fi
 
 #amixer set Capture toggle
