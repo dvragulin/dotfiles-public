8c111ca7cffc3e3621f2b49ae304a8496d20459c
Dmitry Ragulin <dvragulin@gmail.com>
2024-02-06 10:41:36 +0300

    Update polybar config

---
+++ roles/03_Profile/polybar/files/threads.count.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COUNT=$(
+  curl -sS \
+  -X GET \
+  -H "Content-type: application/json; charset=utf-8" \
+  -H "Authorization: Bearer $MATTERMOST_TOKEN" \
+  "$MATTERMOST_HOST/api/v4/users/$MATTERMOST_USER_ID/teams/$MATTERMOST_TEAM_ID/threads" | jq ".total"
+)
+
+if [ $COUNT -gt 50 ]; then echo   %{F\#cebaba}$COUNT ; fi
+
