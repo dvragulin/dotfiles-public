82b6f3d5f00087165519cf505b4ab32487d12fdf
Dmitry Ragulin <dvragulin@gmail.com>
2024-03-03 00:54:45 +0300

    Update polybar config

--- roles/03_Profile/polybar/files/launch.sh
+++ roles/03_Profile/polybar/files/launch.sh
@@ -10,6 +10,26 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar dvragulin &
 dunst -config ~/.config/dunst/dunstrc &
 
-xrandr --output eDP-1 --off
-notify-send -a "Polybar" -u normal "Polybar running..."
+# Конфигурируем запуск на мониторах и отдельно
+connected_screens=$(xrandr | grep -E 'DP-3-2')
+if [[ $connected_screens ]]; then
+  xrandr --output eDP-1 --off
+  xrandr --output DP-3-2 --mode 2560x1440 --pos 0x0 --primary
+  xrandr --output DP-3-3 --mode 2560x1440 --pos 2560x0
+  notify-send -a "xrandr" -u normal "set configuration:
+  eDP-1 is off
+  DP-3-2 set primary
+  DP-3-3
+  "
+else
+  xrandr --output eDP-1 --primary
+  notify-send -a "xrandr" -u normal "set configuration:
+  eDP-1 set primary
+  DP-3-2 is off
+  DP-3-3 is off
+  "
+fi
+
+notify-send -a "Polybar" -u normal "Welcome!"
 yandex-disk start
+
--- roles/03_Profile/polybar/files/mic.sh
+++ roles/03_Profile/polybar/files/mic.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-VAR=$(amixer -c 1| rg "Mono: Capture" | rg "\[on\]" | awk '{print $6}')
+VAR=$(amixer -c 1 2>/dev/null | rg "Mono: Capture" | rg "\[on\]" | awk '{print $6}')
 if  [[ $VAR = "[on]" ]]; then echo ; else echo %{F\#cebaba}; fi
