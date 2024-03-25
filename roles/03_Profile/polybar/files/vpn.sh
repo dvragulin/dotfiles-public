da4a42d0caef94ad072bf9eb02b3e41a108b104b
Dmitry Ragulin <dvragulin@gmail.com>
2024-03-25 15:50:41 +0300

    Update polybar

--- roles/03_Profile/polybar/files/vpn.sh
+++ roles/03_Profile/polybar/files/vpn.sh
@@ -7,7 +7,7 @@ vpn-switcher(){
     echo $ADFS_PASSWORD | sudo openconnect --protocol=gp --os=linux-64 --background --passwd-on-stdin --user=$ADFS_USER_WORK --server=$VPN_WORK_SRV --authgroup=$VPN_WORK_SRV_GROUP
     notify-send -a "VPN" -u normal "Connected"
   elif [[ "$input_str" == "disconnect" ]]; then
-    sudo /usr/bin/kill $(ps aux | rg openconnect | head -1 | awk '{{ print $2}}')
+    sudo /usr/bin/kill $(ps aux | rg openconnect | rg background | awk '{{ print $2}}')
     notify-send -a "VPN" -u normal "Disconnected"
   else
     return 0
@@ -17,4 +17,4 @@ vpn-switcher(){
 vpn-switcher "$1"
 
 VAR=$(ps aux | rg openconnect | rg background | awk '{{ print $2}}')
-if  [[ -z $VAR ]]; then echo 󱕴 ; else echo %{F\#7AB87A}󱕴%{F-}; fi
+if [[ -z $VAR ]]; then echo 󱕴 ; else echo %{F\#7AB87A}󰕥%{F-}; fi
