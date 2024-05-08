65b7c808b2184e5d0af29d7f26d0ddd06acfa73e
Dmitry Ragulin <dvragulin@gmail.com>
2024-05-08 10:29:02 +0300

    Update polybar

--- roles/03_Profile/polybar/files/mattermost.sh
+++ roles/03_Profile/polybar/files/mattermost.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-COUNT_THREADS=$(
-  curl -sS \
+
+# Выполняем запрос с помощью curl
+RESPONSE=$(curl -sS \
   -X GET \
   -H "Content-type: application/json; charset=utf-8" \
   -H "Authorization: Bearer $MATTERMOST_TOKEN" \
-  "$MATTERMOST_HOST/api/v4/users/$MATTERMOST_USER_ID/teams/$MATTERMOST_TEAM_ID/threads" | jq ".total"
-)
+  "$MATTERMOST_HOST/api/v4/users/$MATTERMOST_USER_ID/teams/$MATTERMOST_TEAM_ID/threads")
 
-COUNT_FLAGGED=$(
-  curl -sS \
-  -X GET \
-  -H "Content-type: application/json; charset=utf-8" \
-  -H "Authorization: Bearer $MATTERMOST_TOKEN" \
-  "$MATTERMOST_HOST/api/v4/users/$MATTERMOST_USER_ID/posts/flagged" | jq -r '.order | length'
-)
-
-if [ $COUNT_THREADS -gt 25 ]; then echo "  $COUNT_FLAGGED  %{F#cebaba}  $COUNT_THREADS"; else echo "  $COUNT_FLAGGED    $COUNT_THREADS"; fi
+if [ $? -ne 0 ]; then
+  echo %{F\#913434}󰕥%{F-}
+  exit 0
+else
+  COUNT_THREADS=$(echo "$RESPONSE" | jq ".total")
+  COUNT_FLAGGED=$(
+    curl -sS \
+    -X GET \
+    -H "Content-type: application/json; charset=utf-8" \
+    -H "Authorization: Bearer $MATTERMOST_TOKEN" \
+    "$MATTERMOST_HOST/api/v4/users/$MATTERMOST_USER_ID/posts/flagged" | jq -r '.order | length'
+  )
+  
+  if [ $COUNT_THREADS -gt 25 ]; then echo "  $COUNT_FLAGGED  %{F#cebaba}  $COUNT_THREADS"; else echo "  $COUNT_FLAGGED    $COUNT_THREADS"; fi
+fi
 
